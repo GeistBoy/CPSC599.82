01d026dcf9c570580caec9dcd47bb3355101c9db
Zheng Liang <zhengliangnnc@gmail.com>
2018-10-10 01:01:12 -0600

    Some more test programs

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # CPSC599.82
 This is a project for CPSC 599.92 - Retro Games
 
+usage for test program:
+dasm test.asm -otest.prg
 
+then load the test.prg file into the xvic emulator,
+then type "sys 4352", which means $1100, then press enter,
+Enjoy! 
--- testProgram/test.asm
+++ testProgram/test.asm
@@ -1,5 +1,83 @@
-	processor	6502
-	org	$1100
-loop:
+;This is the test program for the CPSC 599.82
+;Test1 is flashing the screen with changing the Screen and border color register
+;Test2 is print out "HELLO WORLD!" on the screen
+;Test3 is created by mistake. It was originally get keyboard input and print it on the screen but it prints out grids
+
+
+;==============================================================
+;Start of the test programs
+	processor	6502 ;pseudo code for dasm to indicate
+	org	$1100        ;Start point of this program in the memory
+
+;START WHICH TEST? (Just for now, there is another way to switch the routines)
+	jmp	test5
+
+;============================================================
+;Test5 - *** didn't work
+;Trying to output some sound
+test5:
+	lda	#15
+	jsr	$900e
+	lda	226
+	jsr	$900c
+	jsr	$900d
+	jmp	test5
+
+
+;=============================================================
+;Test 4
+;This one is the correct version of test3
+;Which get the input from the keyboard and output on the screen
+test4:
+	jsr	$ffe4	;could change to $ffcf, but got problem when press enter key
+	jsr	$ffd2
+	jmp	test4
+
+;==============================================================
+;Test 3 - ***might delete after
+;This one is trying to read keyboard input but something wrong
+;GETIN, $ffe4 - Get character from keyboar queue(keyboard buffer)
+test3:
+	lda	$ffe4
+	jsr	$ffd2
+	jmp	test3
+
+;==============================================================
+;Test 2
+;This one prints "HELLO WORLD!" on the screen
+;CHROUT $ffd2, Output character to channel
+test2:
+	jsr	$e55f
+	lda	#'H	;H
+	jsr	$ffd2
+	lda	#'E	;E
+	jsr	$ffd2
+	lda	#'L	;L
+	jsr	$ffd2
+	lda	#'L	;L
+	jsr	$ffd2
+	lda	#'O	;O
+	jsr	$ffd2
+	lda	#' 	;space
+	jsr	$ffd2
+	lda	#'W	;W
+	jsr	$ffd2
+	lda	#'O	;O
+	jsr	$ffd2
+	lda	#'R	;R
+	jsr	$ffd2
+	lda	#'L	;L
+	jsr	$ffd2
+	lda	#'D	;D
+	jsr	$ffd2
+	lda	#'!	;!
+	jsr	$ffd2
+	jmp	test2
+
+;=====================================================
+;Test 1
+;This one flashing the screen by
+;changing the Screen and border color register
+test1:
 	inc	$900f
-	jmp	loop
+	jmp	test1
